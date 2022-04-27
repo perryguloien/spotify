d953d0251a6cad61a4a2845b628349ddbf28054a
Perry Guloien <perryaguloien@gmail.com>
2022-04-27 17:46:25 -0400

    working

--- web_app/routes/spotify_routes.py
+++ web_app/routes/spotify_routes.py
@@ -6,16 +6,36 @@ from app.spotify_service import get_similar_artists
 
 from flask import Blueprint, request, jsonify, render_template, redirect, flash # FYI new imports
 
+@spotify.route('/spotify/artist')
+def search_artist(name):
+    #search from the spotipy api
+    results = sp.search(q='artist:' + name, type='artist')
+    items = results['artists']['items']
+    if len(items) > 0:
+        artist = items[0]
+        global artist_id 
+        artist_id = artist['id']
+        print(artist_id)
+        return(artist_id)
+    else:
+        print("There are no results for your input.")
+        quit()
+
+
+
+if __name__ == '__main__':
+   app.run()
+
 spotify_routes = Blueprint("spotify_routes", __name__)
 
-#@spotify_routes.route("/api/spotify.json")
-#def weather_forecast_api():
-#    print("WEATHER FORECAST (API)...")
-#    print("URL PARAMS:", dict(request.args))
-#
-#    # country_code = request.args["country_code"] # the dict might not have this key all the time
-#    country_code = request.args.get("country_code") or "US"
-#    zip_code = request.args.get("zip_code") or "20057"
+@spotify_routes.route("/api/spotify.json")
+def weather_forecast_api():
+    print("WEATHER FORECAST (API)...")
+    print("URL PARAMS:", dict(request.args))
+
+country_code = request.args["country_code"] # the dict might not have this key all the time
+country_code = request.args.get("country_code") or "US"
+zip_code = request.args.get("zip_code") or "20057"
 #
 #    results = get_hourly_forecasts(country_code=country_code, zip_code=zip_code)
 #    if results:
