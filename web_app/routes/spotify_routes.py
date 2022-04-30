ff082eb8e41acdca9c7bf62c6f84b0f9e1482185
basilbseiso <bb1026@georgetown.edu>
2022-04-30 13:57:47 -0400

    Update spotify_routes.py

--- web_app/routes/spotify_routes.py
+++ web_app/routes/spotify_routes.py
@@ -13,16 +13,31 @@ spotify_routes = Blueprint("spotify_routes", __name__)
 def spotify_form():
     return render_template("spotify_form.html")
 
+#@spotify_routes.route("/spotify", methods = ["POST"])
+#def recommend_artist():
+#    print("ARTIST RECOMMENDATIONS..")
+#    request_data = dict(request.form)  
+#    print("Form data:" , request_data)
+#    artist_name = request_data.get("artist_name") or "Dua Lipa"
+#    results = artist_recommendation(name = artist_name)
+#    print(results)
+#    flash("Artist Recommendations Provided Successfully!")
+#    return render_template("spotify_results.html", results = results[0], artist_name = results[1])
+
 @spotify_routes.route("/spotify", methods = ["POST"])
 def recommend_artist():
-    print("ARTIST RECOMMENDATIONS..")
     request_data = dict(request.form)  
-    print("Form data:" , request_data)
     artist_name = request_data.get("artist_name") or "Dua Lipa"
     results = artist_recommendation(name = artist_name)
-    print(results)
-    flash("Artist Recommendations Provided Successfully!")
-    return render_template("spotify_results.html", results = results[0], artist_name = results[1])
+    if results is None:
+        flash("There are no results for your inputted artist. Please try again.")
+        return render_template("spotify_form.html")
+    else:
+        print("ARTIST RECOMMENDATIONS..")
+        print("Form data:" , request_data)
+        print(results)
+        flash("Artist Recommendations Provided Successfully!")
+        return render_template("spotify_results.html", results = results[0], artist_name = results[1])
 
 @spotify_routes.route("/about")
 def about():
