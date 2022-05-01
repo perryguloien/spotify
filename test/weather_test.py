0d064c70a8db006f0459503edb9b3681cf26b4df
Abby Lee <aal86@georgetown.edu>
2022-05-01 19:12:43 -0400

    testing

--- test/weather_test.py
+++ test/weather_test.py
@@ -2,24 +2,17 @@
 import os
 import pytest
 
-from app.weather_service import get_hourly_forecasts, DEGREE_SIGN, format_hour
+from app.spotify_service import artist_recommendation
 
 CI_ENV = os.getenv("CI") == "true"
 
 @pytest.mark.skipif(CI_ENV==True, reason="to avoid issuing HTTP requests on the CI server")
-def test_hourly_forecasts():
-    # with valid geography, returns the city name and forecast info:
-    results = get_hourly_forecasts(country_code="US", zip_code="20057")
-    assert results["city_name"] == "Washington, DC"
-    assert len(results["hourly_forecasts"]) == 24
-    forecast = results["hourly_forecasts"][0]
-    assert sorted(list(forecast.keys())) == ["conditions", "image_url", "temp", "timestamp"]
-    assert forecast["timestamp"].endswith(":00")
-    assert f"{DEGREE_SIGN}F" in forecast["temp"]
+def test_reccommendations():
+    # with valid name, returns the reccommended artists and songs:
+    results = artist_recommendation(name = "Dua Lipa")
+    assert results["artist"] == "Dua Lipa"
 
-    # with invalid geography, fails gracefully and returns nothing:
-    invalid_results = get_hourly_forecasts(country_code="US", zip_code="OOPS")
+    # with invalid name, fails gracefully and returns nothing:
+    invalid_results = artist_recommendation(name=129112)
     assert invalid_results == None
 
-def test_hour_formatting():
-    assert format_hour("2021-03-29T21:00:00-04:00") == "21:00"
