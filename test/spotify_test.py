d4abf59a360f5c05492dc571d8feaeac747f8fbb
Perry Guloien <perryaguloien@gmail.com>
2022-05-02 16:37:45 -0400

    updating testing

--- test/spotify_test.py
+++ test/spotify_test.py
@@ -2,19 +2,32 @@
 import os
 from unicodedata import name
 import pytest
+import spotipy
+import spotipy.oauth2
+import spotipy.util as util
+from spotipy.oauth2 import SpotifyClientCredentials
+from app.spotify_service import artist_recommendation
+#SET VARIABLES WITH ENV INFO
+CLIENT_ID = os.environ.get("CLIENT_ID", "OOPS")
+CLIENT_SECRET = os.environ.get("CLIENT_SECRET", "OOPS")
 
-from app.spotify_service import artist_recommendation 
+#SET UP CODE SO WE CAN USE THE SPOTIPY API DATA
+client_credentials_manager = SpotifyClientCredentials(client_id=CLIENT_ID, client_secret=CLIENT_SECRET)
+sp = spotipy.Spotify(client_credentials_manager=client_credentials_manager)
 
 CI_ENV = os.getenv("CI") == "true"
 
 @pytest.mark.skipif(CI_ENV==True, reason="to avoid issuing HTTP requests on the CI server")
 def test_reccommendations():
-    with valid name, returns the reccommended artists and songs:
+    results = sp.search(q='artist:' + name, type='artist')
+    items = results['artists']['items']
+    if len(items) > 0:
+        artist = items[0]
+    #with valid name, returns the reccommended artists and songs:
     results = artist_recommendation(name = "Dua Lipa")
-    assert results["Dua Lipa"] == [name]
+    assert artist['name']
     
-    with invalid name, fails gracefully and returns nothing:
+    #with invalid name, fails gracefully and returns nothing:
     invalid_results = artist_recommendation(name=129112)
     assert invalid_results == None
 
-#
