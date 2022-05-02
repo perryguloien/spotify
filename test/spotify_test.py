35237642434d8e7c84f2aae0e5dcd1a8b0d33db9
basilbseiso <bb1026@georgetown.edu>
2022-05-02 18:46:51 -0400

    Testing

--- test/spotify_test.py
+++ test/spotify_test.py
@@ -11,10 +11,18 @@ CI_ENV = os.getenv("CI") == "true"
 def test_reccommendations():
     #with valid name, returns the reccommended artists and songs:
     results,artist_name = artist_recommendation("Dua Lipa")
-    assert name_res == 'Dua Lipa'
-    
-    breakpoint()
-    #with invalid name, fails gracefully and returns nothing:
-    invalid_results = artist_recommendation(name=129112)
-    assert invalid_results == None
+    assert artist_name == 'Dua Lipa'
+    artist_type = isinstance(artist_name, str)
+    assert artist_type == True
 
+    #results = artist_recommendation("Dua Lipa")
+    recommendation_type = isinstance(results, list)
+    assert recommendation_type == True
+    
+    related_count = len(results)
+    assert related_count == 5
+
+    related_artist = results[0]
+    assert isinstance(related_artist, dict)
+    keys = list(related_artist.keys())
+    assert keys == ['names', 'display_images', 'songs']
